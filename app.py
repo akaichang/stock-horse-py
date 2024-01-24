b542ee3153e3c9d4980e5f9045aaa94bc781216e
akaichang <waxchang@gmail.com>
2024-01-24 13:30:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,8 +39,22 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
+    message = getCurPrice(message)
     line_bot_api.reply_message(event.reply_token, message)
 
+def getCurPrice(tStock):
+    url = f"https://tw.quote.finance.yahoo.net/quote/q?type=ta&perd=d&mkt=10&sym={tStock}&v=1&callback=jQuery111302872649618000682_1649814120914&_=1649814120915"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/111.25 (KHTML, like Gecko) Chrome/99.0.2345.81 Safari/123.36'}
+    res = requests.get(url,headers=headers)
+    # 最新價格
+    current = [l for l in res.text.split('{') if len(l)>=60][-1]
+    current = current.replace('"','').split(',')
+    #print(res.text.split('{')[2])
+    print(res.text.replace('"','').split('{')[2].split(',')[1].split(':')[1])
+    # 昨日價格
+    #yday = float(re.search(':.*',[l for l in res.text.split('{') if len(l)>=60][-2].split(',')[4]).group()[1:])
+    return float(re.search(':.*',current[4]).group()[1:])
+    
 #import os
 if __name__ == "__main__":
     #port = int(os.environ.get('PORT', 8000))
